1027c5959bd34529ce6d147d71112a92c9f964ae
Akash KC <akash.kc3745@gmail.com>
2026-05-08 23:05:53 +0100

    fix: Desktop topic

--- Desktop.py
+++ Desktop.py
@@ -1,13 +1,19 @@
-from tkinter import *
-dsk=Tk()
+from tkinter import Tk, Label
+
+dsk = Tk()
 dsk.title("pyapp")
 dsk.geometry("600x500")
-dsk.maxsize(600,500)
-dsk.minsize(300,300)
-label1=Label(text="welcome to my app",bg="red",fg="yellow",font="helvetica 30 bold", padx="50",pady="50")
+dsk.maxsize(600, 500)
+dsk.minsize(300, 300)
+label1 = Label(
+    text="welcome to my app",
+    bg="red",
+    fg="yellow",
+    font="helvetica 30 bold",
+    padx="50",
+    pady="50",
+)
 # label1.pack(side=LEFT,fill="x")
 label1.pack(fill="x")
 dsk.config(background="cyan")
 dsk.mainloop()
-
-
